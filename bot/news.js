0cf406ab90c0698ce17ba982fc81b03461f7daa7
Hail Santa <gembarsur@outlook.com>
2026-03-18 11:15:27 +0700

    Update news.js

--- bot/news.js
+++ bot/news.js
@@ -1,21 +1,14 @@
 const axios = require("axios")
 
-async function getGlobalNews(query = "world"){
-
-    const key = process.env.GNEWS_API_KEY
+async function getGlobalNews(query="world"){
 
     const res = await axios.get(
-        `https://gnews.io/api/v4/search?q=${encodeURIComponent(query)}&lang=en&max=5&apikey=${key}`
+        `https://gnews.io/api/v4/search?q=${encodeURIComponent(query)}&lang=en&max=5&apikey=${process.env.GNEWS_API_KEY}`
     )
 
     const articles = res.data?.articles || []
 
-    return articles.map(a => ({
-        title: a.title,
-        source: a.source?.name,
-        url: a.url,
-        time: a.publishedAt
-    }))
+    return articles.map(a => a.title)
 }
 
 module.exports = { getGlobalNews }
